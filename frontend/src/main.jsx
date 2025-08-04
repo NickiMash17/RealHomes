609e341daac6294f7f9c1c8434a532c442796ce1
NickiMash17 <nicolette.mashaba@techbridlefoundation.org>
2025-08-04 02:23:20 +0200

    fix: rename MockAuthContext.js to .jsx extension for JSX syntax

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import "@mantine/core/styles.css";
 import "@mantine/dates/styles.css";
-import { MockAuthProvider } from './context/MockAuthContext'
+import { MockAuthProvider } from './context/MockAuthContext.jsx'
 import { MantineProvider } from '@mantine/core';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
