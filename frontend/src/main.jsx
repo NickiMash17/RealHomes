1ca563b6f90b8acfb2c0c19e94511a0143307bd7
Nicolette Mashaba <nicolette.mashaba@techbridlefoundation.org>
2025-12-13 17:02:59 +0200

    fix: Force light mode with MutationObserver to prevent any dark mode toggle or system preferences

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -48,12 +48,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </MockAuthProvider>
   </React.StrictMode>
 )
-import { MockAuthProvider } from './context/MockAuthContext.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <MockAuthProvider>
-      <App />
-    </MockAuthProvider>
-  </React.StrictMode>
-)
